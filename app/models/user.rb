fd1ddbd6eb7e0da3c5b68c19c6c70dfc39f5fb16
FerdousNasri <itrytocode@gmail.com>
2015-04-27 22:09:43 +0200

    Add uniqueness validation to email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ActiveRecord::Base
 	validates :location, presence: true
 	validates :twitter_handle, format: { with: /\A\w+\z/, message: "%{value} is not a valid twitter handle!"}
 	validates :name, presence: true
-  	validates :email, format: {with: /\A[^@]+@[^@]+\.[^@]+\z/}
+  validates :email, format: {with: /\A[^@]+@[^@]+\.[^@]+\z/}
+	validates :email, uniqueness: true
 end
-
-
