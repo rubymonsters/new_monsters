7e7c3a7ab6133932b8933b8948e4a49ad9dcc8af
Ruby on Rails <lang.joh.jl@gmail.com>
2015-04-27 21:43:29 +0200

    Addes validations to location and twitterhandle in the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ActiveRecord::Base
+	validates :location, presence: true
+	validates :twitter_handle, format: { with: /\A\w+\z/, message: "%{value} is not a valid twitter handle!"}
 end
