24668b8864febf70bfa9865acf92ceb4b5047c1f
Ruby on Rails <lang.joh.jl@gmail.com>
2015-05-04 21:59:44 +0200

    Action view form error messages.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
-	validates :location, presence: true
-	validates :twitter_handle, format: { with: /\A\w+\z/, message: "%{value} is not a valid twitter handle!"}
-	validates :name, presence: true
+  validates :location, presence: true
+  validates :twitter_handle, format: { with: /\A\w+\z/, message: "%{value} is not a valid twitter handle!"}
+  validates :name, presence: true
   validates :email, format: {with: /\A[^@]+@[^@]+\.[^@]+\z/}
-	validates :email, uniqueness: true
+  validates :email, uniqueness: true
 end
