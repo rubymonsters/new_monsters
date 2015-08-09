bab829966caf56d0f0172ac2154652ac6f26f932
FerdousNasri <itrytocode@gmail.com>
2015-08-09 23:39:02 +0200

    Update verifications
    - optimised Email verification
    - added verification formats for location
    - optimised twitter handle verifications

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,16 @@
 class User < ActiveRecord::Base
-	validates :location, presence: true
-	validates :twitter_handle, format: { allow_blank: true, with: /\A\w+\z/, message: "%{value} is not a valid twitter handle!"}
-	validates :twitter_handle, uniqueness: true, presence: true
 	validates :name, presence: true
-  validates :email, format: { allow_blank: true, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create}
+	# validates :name, presence: { strict: true }
+	# what's the difference?
+
+	validates :email, format: { allow_blank: true, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i}
 	validates :email, uniqueness: true, presence: true
+
+	validates :location, presence: true
+	# ERROR: how can I make it ignore spaces? (with /x ?)
+	validates :location, format: { allow_blank: true, with: /\A[a-zA-Z]+\z/i }
+
+	validates :twitter_handle, format: { allow_blank: true, with: /\A\w+\z/i, message: "%{value} is not a valid twitter handle!"}
+	# when present, check for uniqueness
+	validates :twitter_handle, uniqueness: true, if: 'twitter_handle.present?'
 end
